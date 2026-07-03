78fcac967bc4fa462b075f30e5f72b46309e8852
maykima9-hub <may.kima9@gmail.com>
2026-07-03 12:32:32 +0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://wxhffynpmyounltopsgz.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_JvKYZyFSQtjmw-0owzp20w_oF6HsuGS";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
